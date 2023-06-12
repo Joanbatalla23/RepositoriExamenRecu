98a6236b9c6fe1ca6ffc4775bacd187708aee2f1
Joanbatalla23 <106758067+Joanbatalla23@users.noreply.github.com>
2023-06-12 16:18:29 +0200

    Examen.java

--- Examen.java
+++ Examen.java
@@ -1,6 +1,6 @@
 public class Examen {
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 	
 		System.out.println("Soc el Joan Batalla Camenforte i estic fent l'examen de recuperacio de la UF2 del M01");
 	
