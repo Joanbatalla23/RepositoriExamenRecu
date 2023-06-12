a87aee33f9a78ed8de0b5622ee33081677edf31a
Joanbatalla23 <106758067+Joanbatalla23@users.noreply.github.com>
2023-06-12 16:02:36 +0200

    Add files via upload

---
+++ Examen.java
@@ -0,0 +1,8 @@
+public class Examen {
+	
+	public static void main(String[] args){
+	
+		System.out.println("Soc el Joan Batalla Camenforte i estic fent l'examen de recuperacio de la UF2 del M01")
+	
+	}
+}
